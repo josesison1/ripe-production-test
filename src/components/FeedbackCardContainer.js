6db55c123fd3872e8607c5302e46b6d9ea8cdb32
keelanpattersoncaesar <keelanpattersoncaesar@gmail.com>
2022-05-18 16:52:59 -0700

    added time, card styling

--- src/components/FeedbackCardContainer.js
+++ src/components/FeedbackCardContainer.js
@@ -2,26 +2,17 @@ import React from 'react'
 import { test } from '../testObject'
 
 const FeedbackCardContainer = () => {
+const timestamp = Date.now();
+const timeOnly = new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp);
+// console.log(timestamp)
   return (
     <>
-    <div className="max-w-2xl h-2xl rounded overflow-hidden shadow-lg">
-      <img className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{test.cardType}</div>
-        <p className="text-gray-700 text-base">
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
-        </p>
+    <div className="flex flex-row max-w-2xl h-2xl bg-orange-200 rounded overflow-hidden shadow-lg">
+      <div className="px-6 py-4 w-full">
+        <div className="font-bold text-xl mb-2 border-2 border-black w-full flex justify-center h-screen items-center">
+        <span>{test.cardType}</span> 
+          <span>{timeOnly}</span>
+        </div>
       </div>
     </div>
     </> 
