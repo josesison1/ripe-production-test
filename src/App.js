580aa6e82be0be7a5c830ba8e1ece89ba32f176d
josesison <josesison1@yahoo.com>
2022-05-18 11:53:18 -0700

    updated app, added testObject, added feedback card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { test } from './testObject'
+import FeedbackCardContainer from './components/FeedbackCardContainer'
 
-function App() {
+const App = () => {
+  const { surveyQuestions } = test
+  console.log(surveyQuestions)
   return (
     <>
     <h1>Hello World!</h1>
+    {test.customerNme}
+    {surveyQuestions.map(v => v.surveyQuestion)}
+    <FeedbackCardContainer />
     </>
   )
 }
