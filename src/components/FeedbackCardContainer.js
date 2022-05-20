78cf4d55b26aff86a246a088f63e123cf2ff8bf0
Tiffany Do <tiffanydoh@gmail.com>
2022-05-20 13:42:40 -0700

    added feedback body

--- src/components/FeedbackCardContainer.js
+++ src/components/FeedbackCardContainer.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import { test } from '../testObject'
+import FeedbackCardHeader from './FeedbackCardHeader'
+import FeedbackCardBody from './FeedbackCardBody'
 
 const FeedbackCardContainer = () => {
-const timestamp = Date.now();
-const timeOnly = new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp);
-// console.log(timestamp)
   return (
     <>
-    <div className="flex flex-row max-w-2xl h-2xl bg-orange-200 rounded overflow-hidden shadow-lg">
-      <div className="px-6 py-4 w-full">
-        <div className="font-bold text-xl mb-2 border-2 border-black w-full flex justify-center h-screen items-center">
-        <span>{test.cardType}</span> 
-          <span>{timeOnly}</span>
+      <div className="flex flex-row max-w-2xl h-2xl bg-orange-200 rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4 w-full">
+          <div className="font-bold text-xl mb-2 w-full">
+            <FeedbackCardHeader />
+            <div className="border-2 border-orange-400">
+              <FeedbackCardBody />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </> 
-    )
+    </>
+  )
 }
 
 export default FeedbackCardContainer
---
+++ src/views/FeedbackBodyAnswers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { test } from '../testObject'
+
+const FeedbackBodyAnswers = () => {
+  const { surveyQuestions } = test
+  console.log(surveyQuestions)
+  return (
+    <>
+      <div>
+        <div className="p-5 pb-4">
+          {surveyQuestions.map(v => <li className="list-none"> {v.surveyQuestionAnswer} </li>)}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FeedbackBodyAnswers
