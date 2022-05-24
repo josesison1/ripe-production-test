b2afc41cae194ed20fa31e8c136c3152a0cc700f
josesison <josesison1@yahoo.com>
2022-05-24 16:13:27 -0700

    updated functionality for tags

--- src/views/FeedbackFooterTags.js
+++ src/views/FeedbackFooterTags.js
@@ -5,32 +5,21 @@ import { GrFormClose } from 'react-icons/gr'
 import { useState } from 'react'
 
 const FeedbackFooterTags = () => {
-  const { tags } = test
   const [tagValue, setTagValue] = useState('');
   const [addTag, setAddTag] = useState(test.tags)
+  // const { tags } = addTag
 
-  // const saveTagValue = (e) => {
-  //   if(e.key === 'Enter') {
-  //     setTagValue[e.target.tagValue] = e.target.value
-  //     e.target.value = '' 
-  //   } 
-  // }
-  console.log(tagValue);
-
-  const addTags = () => {
-    const newValue = setTagValue; 
-    setAddTag(newValue.push())
-  }
-
-  // console.log(test.tags);
+    const addTags = () => {
+      setAddTag([...addTag, tagValue])
+    }
 
   return (
     <>
       <div className='flex justify-between flex-wrap'>
         <div className='flex mx-2'>
-          {tags.map(v => {
+          {addTag.map((v, index) => {
             return (
-              <div className='flex justify-between border-2 border-orange-400 border-w mr-2 text-sm p-2 px-2 bg-white text-orange-400 rounded-full'>{v}
+              <div key={index} className='flex justify-between border-2 border-orange-400 border-w mr-2 text-sm p-2 px-2 bg-white text-orange-400 rounded-full'>{v}
                 <div className='mx-2'> <GrFormClose/> </div>
               </div>
             )
